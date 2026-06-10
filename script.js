0e761fbc15119bcd2868517b2ad4372338f24e6a
GUILDER <pplg39395@gmail.com>
2026-06-10 13:40:07 -0400

    Cambio al streaming real de LaNuevaLatina

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const sidebarLinks = document.querySelectorAll(".sidebar-nav a");
 const aboutToggle = document.getElementById("aboutToggle");
 const aboutMore = document.getElementById("aboutMore");
 
-const streamUrl = "https://stream-02.surfernetwork.com/sfqwqh7vd6quv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJzZnF3cWg3dmQ2cXV2IiwiaG9zdCI6InN0cmVhbS0wMi5zdXJmZXJuZXR3b3JrLmNvbSIsInJ0dGwiOjUsImp0aSI6Ijg1SXZyM2N5U1VhbUNBNlN5ZUJHNUEiLCJpYXQiOjE3NzkzNjk5NjEsImV4cCI6MTc3OTM3MDAyMX0.KrBt2NBm95UItrMElVLnTzmIrNK_rP3KwkDaV7b3GrE&cto-uid=928323b8-fb3a-4c04-9418-24ba93474de6-66cf8cb1-424f&acu-uid=1463883372361&an-uid=6590601941648504964&dot-uid=0d2f2204007fc6007f3535a8&triton-uid=cookie%3A2a7bf438-d03c-43f7-9592-ce02e0e3338b&amb-uid=8290782153782580478&dbm-uid=CAESEDXcVa4TM5l1ttTw8P9wUWs&dyn-uid=1838828465535879092&ttd-uid=7f7a1b71-eab6-4caf-99dd-a160feafc55e&aw_0_req_lsid=f300d633caf8460738d97d65e4487104";
+const streamUrl = "https://cast3.my-control-panel.com/proxy/lanueval/stream";
 
 radioPlayer.src = streamUrl;
 radioPlayer.volume = Number(volumeRange.value) / 100;
